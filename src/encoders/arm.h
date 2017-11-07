89ca1ee476a65ff3df1cb67cfefb317632f8cd10
Colin James <obscurecolin@gmail.com>
2017-11-07 14:11:13 +0000

    Added instruction encoding masks (relevant ones)

--- src/encoders/arm.h
+++ src/encoders/arm.h
@@ -36,6 +36,27 @@ enum class arm_cond : uint8_t {
  */
 std::string arm_cond_to_str(const arm_cond cond);
 
+/**
+ * ARM instruciton group masks (only accomodationg relevant ones for ROP and filtering)
+ */
+enum class arm_mask : uint32_t {
+  /**
+   * Branch and exchange
+   *
+   */
+      BX = 0b00000001001011111111111100010000,
+  /**
+   * Single load/store
+   * The 20th bit of this being set (1) indicates a load.
+   */
+      LDR = 0b0000010000000000000000000000000,
+  /**
+   * Load store block (multiple, e.g. ldmfd sp!, {r0-r3, pc})
+   * The 20th bit of this being set (1) indicates a load.
+   */
+      LDM = 0b00001000000000000000000000000000
+};
+
 } // namespace arm
 
 #endif //ARMOUR_ARM_H
