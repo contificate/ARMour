fc341e9a5471069d54db32c20b2c7f26c70e9cd8
Colin James <obscurecolin@gmail.com>
2017-11-08 12:36:54 +0000

    Committed style guide hacks

--- src/encoders/arm_encoder.cc
+++ src/encoders/arm_encoder.cc
@@ -6,8 +6,17 @@ arm_cond ARMEncoder::cond() const {
   return static_cast<arm_cond>(value_at(28, 4));
 }
 
+ARMEncoder::operator u_type() const {
+  return value_;
+}
+
 bool ARMEncoder::operator&(const arm_cond cond) const {
   return (value_at(28, 4) == static_cast<uint8_t>(cond));
+}
+
+bool ARMEncoder::operator&(const arm_mask instr_mask) const {
+  const uint32_t kMaskValue = static_cast<uint32_t>(instr_mask);
+  return ((value_ & kMaskValue) == kMaskValue);
 }
 
 } // namespace enc
